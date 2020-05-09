27a2c5135dbdd14e7d446852847147254316fdb8
janakachinthana <janakachinthana1@gmail.com>
2020-05-09 11:14:35 +0530

    project cleaning

--- src/app/shared/destination.model.ts
+++
@@ -1,9 +0,0 @@
-export class Destination {
-    DestinationID : number;
-    DestinationName : String;
-    Time : String;
-    EntranceFee : number;
-    City : String;
-    Rules : String;
-    Description : String;
-}
--- src/app/shared/prevelent.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Prevelent } from './prevelent.model';
-
-describe('Prevelent', () => {
-  it('should create an instance', () => {
-    expect(new Prevelent()).toBeTruthy();
-  });
-});
