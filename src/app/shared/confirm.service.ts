ce4f8204c7411a2fd49c5636116a8cbbfb23d26a
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2020-05-17 00:50:31 +0530

    Changed hotel form interfaces

--- src/app/shared/confirm.service.ts
+++ src/app/shared/confirm.service.ts
@@ -9,7 +9,9 @@ export class ConfirmService {
   list : Home[];
 
   // readonly rootURL = "https://localhost:44392/api"
-  readonly rootURL = "http://localhost:62083/api"
+  //readonly rootURL = "http://localhost:62083/api"
+  readonly rootURL = "https://localhost:44357/api"
+
   constructor(private http : HttpClient) { }
 
 
