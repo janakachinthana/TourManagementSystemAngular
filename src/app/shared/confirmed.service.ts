199ff51d4167ebabb8cafd206ca79caee23c09b4
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-05-16 15:54:20 +0530

    root changes

--- src/app/shared/confirmed.service.ts
+++ src/app/shared/confirmed.service.ts
@@ -8,7 +8,8 @@ export class ConfirmedService {
   formData : Home;
   list : Home[];
 
-  readonly rootURL = "https://localhost:44392/api"
+  // readonly rootURL = "https://localhost:44392/api"
+  readonly rootURL = "http://localhost:62083/api"
   constructor(private http : HttpClient) { }
 
 
