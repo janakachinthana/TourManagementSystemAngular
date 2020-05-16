efdb4a4e1a05f9cff6a2d1ccc97799de103bd0c8
janakachinthana <janakachinthana1@gmail.com>
2020-05-16 22:20:47 +0530

    Guide Managemet re-build

--- src/app/shared/confirmed.service.ts
+++ src/app/shared/confirmed.service.ts
@@ -11,6 +11,7 @@ export class ConfirmedService {
   // readonly rootURL = "https://localhost:44392/api"
   //readonly rootURL = "http://localhost:62083/api"
   readonly rootURL = "https://localhost:44357/api"
+  
   constructor(private http : HttpClient) { }
 
 
