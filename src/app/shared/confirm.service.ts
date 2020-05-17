cf596553165b2d9e41c698f070195fa300879690
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-05-17 14:11:17 +0530

    srvice

--- src/app/shared/confirm.service.ts
+++ src/app/shared/confirm.service.ts
@@ -8,31 +8,8 @@ export class ConfirmService {
   formData : Home;
   list : Home[];
 
-<<<<<<< HEAD
   // readonly rootURL = "https://localhost:44392/api"
   readonly rootURL = "http://localhost:62083/api"
-=======
-<<<<<<< HEAD
-  // readonly rootURL = "https://localhost:44392/api"
-<<<<<<< HEAD
-  readonly rootURL = "https://localhost:44352/api"
-=======
-  //readonly rootURL = "http://localhost:62083/api"
-  readonly rootURL = "https://localhost:44357/api"
-
-=======
-<<<<<<< HEAD
-  // readonly rootURL = "https://localhost:44392/api"
-  // readonly rootURL = "http://localhost:62083/api"
-  readonly rootURL = "https://localhost:44364/api"
-
-
-=======
-  readonly rootURL = "https://localhost:44392/api"
->>>>>>> 8698b74a41e04679644a160624966768eb6f6584
->>>>>>> 5386ca679714ee0a1a0d91279d361fb8cce41fa5
->>>>>>> b1454485ae4db31f32d1d16ce5c1d5ac655bc344
->>>>>>> dd5afd772284e20301a2be13b7626df9f51045c1
   constructor(private http : HttpClient) { }
 
 
--- src/app/shared/destination.service.ts
+++ src/app/shared/destination.service.ts
@@ -10,30 +10,9 @@ export class DestinationService {
 
   formData : Destination;
   list : Destination[];
-<<<<<<< HEAD
   // readonly rootURL = "https://localhost:44392/api"
   readonly rootURL = "http://localhost:62083/api"
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-  // readonly rootURL = "https://localhost:44392/api"
-<<<<<<< HEAD
-  readonly rootURL = "https://localhost:44352/api"
-=======
-  // readonly rootURL = "http://localhost:62083/api"
-  readonly rootURL = "https://localhost:44364/api"
->>>>>>> b1454485ae4db31f32d1d16ce5c1d5ac655bc344
 
-=======
-  readonly rootURL = "https://localhost:44392/api"
->>>>>>> 8698b74a41e04679644a160624966768eb6f6584
->>>>>>> 5386ca679714ee0a1a0d91279d361fb8cce41fa5
->>>>>>> dd5afd772284e20301a2be13b7626df9f51045c1
-
-  // readonly rootURL = "https://localhost:44392/api"
-  //readonly rootURL = "http://localhost:62083/api"
-  readonly rootURL = "https://localhost:44357/api"
   constructor(private http : HttpClient) { }
 
   postDestination(formData : Destination){
